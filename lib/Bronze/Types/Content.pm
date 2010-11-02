6635fa4e0d0858c17a4d709d03268b1faa8170f2
Daniel Ruoso <daniel@ruoso.com>
2010-11-02 00:22:30 -0300

    Content is the base for all of the actual content

---
+++ lib/Bronze/Types/Content.pm
@@ -0,0 +1,27 @@
+package Bronze::Types::Content;
+use Moose;
+extends 'Bronze::Types::Data';
+
+=head1 NAME
+
+Bronze::Type::Content - Superclass for the actual content
+
+=head1 DESCRIPTION
+
+This class is the superclass for all the content types in bronze.
+
+=head1 HIERARCHY
+
+Bronze::Types::Content isa L<Bronze::Types::Data>
+
+1;
+
+__END__
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2010 Daniel Ruoso <daniel@ruoso.com>
+
+This module is distributed under the same terms of Perl itself.
